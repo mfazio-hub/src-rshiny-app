17c0598b7b842855c1e8f897edc4aa080a057ec4
Mike Fazio <michaelf@santarosa.fl.gov>
2025-05-15 08:55:49 -0500

    added site selector and depth selector

--- app.R
+++ app.R
@@ -21,24 +21,29 @@ ui <- page_sidebar(
             checkboxGroupInput("sites",
                                label = "Select Site(s)",
                                choices = unique(df$SiteID),
-                               )
+                               selected = unique(df$SiteID)
+                               ),
             
-            ),
+            radioButtons("depth",
+                         label = "Select Depth",
+                         choices = unique(df$RelativeDepth)
+            )
+    ),
   
-  card(plotOutput("boxplot"))
   
+  mainPanel(
+    card(plotOutput("boxplot"))
     
-
-  
-  
   )
+)
 
 server <- function(input, output, session) {
   filtered_data <- reactive({
-    df %>% filter(site %in% input$sites)
+    df %>% filter(SiteID %in% input$sites)
+    df %>% filter(RelativeDepth %in% input$depth)
   })
   
-  req(input$sites) # require at least one site to be selected
+  #req(input$sites) # require at least one site to be selected
   
   output$boxplot <- renderPlot({
       ggplot(filtered_data(), aes(x = SiteID, y = .data[[input$var]])) +
